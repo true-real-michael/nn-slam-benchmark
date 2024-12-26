e12874689f2212edf4c03cebcf64f03c4f704cf5
Mikhail Kiselyov <kiselev.0353@gmail.com>
2024-12-26 17:54:11 +0300

    better structure

--- aero_vloc/model_conversion/rk3588.py
+++ aero_vloc/model_conversion/rk3588.py
@@ -1,39 +1,47 @@
+import torch
+
+from abc import ABC, abstractmethod
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Tuple
 
-import torch
 from rknn.api import RKNN
 
 
-def convert(
-    model: Path,
-    shape: Tuple[int, int],
-    output: Path,
-    quantization_dataset: Path | None = None,
-):
-    input_shape = [1, 3, shape[0], shape[1]]
+class RknnExportable(ABC):
+    @abstractmethod
+    def export_torchscript(output: Path):
+        pass
+    
+    def export_rknn(self, output: Path):
+        """
+        Export the model to the RKNN format.
+        :param output: The path to save the model
+        """
+        with NamedTemporaryFile(suffix='.pt') as file:
+            self.export_torchscript(file.name)
+            
+            input_shape = [1, 3, self.resize, self.resize]
+            rknn = RKNN(verbose=True)
+            rknn.config(
+                mean_values=[124.16, 116.736, 103.936],
+                std_values=[58.624, 57.344, 57.6],
+                target_platform="rk3588",
+            )
 
-    rknn = RKNN(verbose=True)
-    rknn.config(
-        mean_values=[124.16, 116.736, 103.936],
-        std_values=[58.624, 57.344, 57.6],
-        target_platform="rk3588",
-    )
+            ret = rknn.load_pytorch(model=file.name, input_size_list=[input_shape])
 
-    ret = rknn.load_pytorch(model=model, input_size_list=[input_shape])
+            if ret != 0:
+                raise RuntimeError("Failed to load model")
 
-    if ret != 0:
-        raise RuntimeError("Failed to load model")
+            if quantization_dataset is not None:
+                ret = rknn.build(do_quantization=True, dataset=quantization_dataset)
+            else:
+                ret = rknn.build(do_quantization=False)
+            if ret != 0:
+                raise RuntimeError("Failed to build model")
 
-    if quantization_dataset is not None:
-        ret = rknn.build(do_quantization=True, dataset=quantization_dataset)
-    else:
-        ret = rknn.build(do_quantization=False)
-    if ret != 0:
-        raise RuntimeError("Failed to build model")
-
-    ret = rknn.export_rknn(str(output))
-    if ret != 0:
-        raise RuntimeError("Export rknn model failed!")
+            ret = rknn.export_rknn(str(output))
+            if ret != 0:
+                raise RuntimeError("Export rknn model failed!")
 
