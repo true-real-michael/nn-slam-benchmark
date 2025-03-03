db29004120dc2158e8f203a3e0466304d6c5c10d
Mikhail Kiselyov <kiselev.0353@gmail.com>
2025-03-04 02:09:44 +0300

    fix

--- nnsb/backend/torchscript.py
+++ nnsb/backend/torchscript.py
@@ -5,7 +5,9 @@ import torch
 
 class TorchscriptBackend(Backend):
     def __init__(self, model_path: Path):
-        self.model = torch.jit.load(model_path)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = torch.jit.load(model_path).to(self.device).eval()
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
+        with torch.no_grad():
+            return self.model(x.to(self.device))
