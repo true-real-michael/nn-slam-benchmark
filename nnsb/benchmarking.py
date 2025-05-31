3f891d0f0ce87c3fa4a1413a67d7a5517d17bcd0
Mikhail Kiselyov <kiselev.0353@gmail.com>
2025-06-01 01:06:04 +0300

    warmup

--- nnsb/benchmarking.py
+++ nnsb/benchmarking.py
@@ -11,7 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from itertools import repeat
 from random import sample
 from timeit import default_timer as timer
 from typing import Any, Dict, List, Tuple
